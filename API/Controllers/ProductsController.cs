40ed4daf0252fcc52d4ba2c7f6afe1fb383ba71c
kyxyung1231 <keyyung.cheah@yycadvisors.com>
2025-07-05 13:15:54 +0800

    End of section 2

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,70 @@
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly StoreContext context;
+    
+    public ProductsController(StoreContext context)
+    {
+        this.context = context;
+    }
+    
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+    {
+        return Ok(await context.Products.ToListAsync());
+    }
+    
+    [HttpGet("{id:int}")] // api/products/2
+    public async Task<ActionResult<Product>> GetProduct(int id)
+    {
+        var product = await context.Products.FindAsync(id);
+        if (product == null) return NotFound();
+        
+        return Ok(product);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Product>> CreateProduct(Product product)
+    {
+        context.Products.Add(product);
+        await context.SaveChangesAsync();
+        
+        return Ok(product);
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<ActionResult<Product>> UpdateProduct(int id, Product product)
+    {
+        if (!ProductExists(id) || id != product.Id) return NotFound("Cannot update this product");
+        
+        context.Entry(product).State = EntityState.Modified;
+        await context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<ActionResult> DeleteProduct(int id)
+    {
+        var product = await context.Products.FindAsync(id);
+        if (product == null) return NotFound("Cannot delete this product");
+        
+        context.Products.Remove(product);
+        await context.SaveChangesAsync();
+        
+        return NoContent();
+    }
+
+    private bool ProductExists(int id)
+    {
+        return context.Products.Any(x => x.Id == id);
+    }
+}
